49aad28849766ac1df6cce18d7ef83928246d2f3
Josh Pollock <josh@calderawp.com>
2019-07-10 15:28:29 +0000

    less than ideal tests

--- src/components/DisplayValue.test.js
+++ src/components/DisplayValue.test.js
@@ -5,11 +5,20 @@ import TestRenderer from 'react-test-renderer';
 //Import component to test
 import {DisplayValue} from './DisplayValue';
 
-test( 'Component renders value', () => {
 
+test( 'Component renders value', () => {
+    const value = 'The Value';
+    const testRenderer = TestRenderer.create(<DisplayValue value={value} />);
+    const testInstance = testRenderer.root;
+    expect(testInstance.findByType('div').props.children).toBe(value);
 });
 
-test( 'Component has supplied class name', () => {
 
+test( 'Component has supplied class name', () => {
+    const value = 'The Value';
+    const className = 'hi-roy'
+    const testRenderer = TestRenderer.create(<DisplayValue value={value} className={className}  />);
+    const testInstance = testRenderer.root;
+    expect(testInstance.findByType('div').props.className).toBe(className);
 });
 
