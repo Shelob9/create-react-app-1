1eb174bb0d635505d28c6a8f8680e23fcfca3c18
Josh Pollock <josh@calderawp.com>
2019-07-10 15:33:21 +0000

    Use a snapshot test for the

--- src/components/DisplayValue.test.js
+++ src/components/DisplayValue.test.js
@@ -5,20 +5,16 @@ import TestRenderer from 'react-test-renderer';
 //Import component to test
 import {DisplayValue} from './DisplayValue';
 
-
-test( 'Component renders value', () => {
-    const value = 'The Value';
-    const testRenderer = TestRenderer.create(<DisplayValue value={value} />);
-    const testInstance = testRenderer.root;
-    expect(testInstance.findByType('div').props.children).toBe(value);
+/**
+ * Test all props are in the right place.
+ */
+test( 'Component renders correctly', () => {
+    expect(
+        TestRenderer.create(
+            <DisplayValue
+                value={'The Value'}
+                className={'the-class-name'}
+             />
+        ).toJSON()
+    ).toMatchSnapshot();
 });
-
-
-test( 'Component has supplied class name', () => {
-    const value = 'The Value';
-    const className = 'hi-roy'
-    const testRenderer = TestRenderer.create(<DisplayValue value={value} className={className}  />);
-    const testInstance = testRenderer.root;
-    expect(testInstance.findByType('div').props.className).toBe(className);
-});
-
